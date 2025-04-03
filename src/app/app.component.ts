a753bc219cf8403294c85b88c6a61856de1c4df7
federico-potrich <federico.potrich@mat.tn.it>
2025-04-03 21:41:16 +0200

    aggiunta struttura del menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,92 +5,104 @@ import { ButtonModule } from 'primeng/button';
 import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  imports: [ButtonModule, PanelMenuModule, PanelMenu],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [ButtonModule, PanelMenuModule, PanelMenu],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  items: MenuItem[] | undefined;
+    items: MenuItem[] | undefined;
 
-  ngOnInit(){
-    this.items = [
-      {
-          label: 'Files',
-          icon: 'pi pi-file',
-          items: [
-              {
-                  label: 'Documents',
-                  icon: 'pi pi-file',
-                  items: [
-                      {
-                          label: 'Invoices',
-                          icon: 'pi pi-file-pdf',
-                          items: [
-                              {
-                                  label: 'Pending',
-                                  icon: 'pi pi-stop'
-                              },
-                              {
-                                  label: 'Paid',
-                                  icon: 'pi pi-check-circle'
-                              }
-                          ]
-                      },
-                      {
-                          label: 'Clients',
-                          icon: 'pi pi-users'
-                      }
-                  ]
-              },
-              {
-                  label: 'Images',
-                  icon: 'pi pi-image',
-                  items: [
-                      {
-                          label: 'Logos',
-                          icon: 'pi pi-image'
-                      }
-                  ]
-              }
-          ]
-      },
-      {
-          label: 'Cloud',
-          icon: 'pi pi-cloud',
-          items: [
-              {
-                  label: 'Upload',
-                  icon: 'pi pi-cloud-upload'
-              },
-              {
-                  label: 'Download',
-                  icon: 'pi pi-cloud-download'
-              },
-              {
-                  label: 'Sync',
-                  icon: 'pi pi-refresh'
-              }
-          ]
-      },
-      {
-          label: 'Devices',
-          icon: 'pi pi-desktop',
-          items: [
-              {
-                  label: 'Phone',
-                  icon: 'pi pi-mobile'
-              },
-              {
-                  label: 'Desktop',
-                  icon: 'pi pi-desktop'
-              },
-              {
-                  label: 'Tablet',
-                  icon: 'pi pi-tablet'
-              }
-          ]
-      }
-  ]
-  }
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Classes-Subclasses',
+                icon: 'pi pi-file',
+                items: [
+                    {
+                        label: 'Classes',
+                        icon: 'pi pi-file',
+                    },
+                    {
+                        label: 'Subclasses',
+                        icon: 'pi pi-image',
+                    }
+                ]
+            },
+            {
+                label: 'Races-Subraces',
+                icon: 'pi pi-cloud',
+                items: [
+                    {
+                        label: 'Races',
+                        icon: 'pi pi-cloud-upload'
+                    },
+                    {
+                        label: 'Subraces',
+                        icon: 'pi pi-cloud-download'
+                    }
+                ]
+            },
+            {
+                label: 'Monster',
+                icon: 'pi pi-desktop'
+            },
+            {
+                label: 'Spells',
+                icon: 'pi pi-desktop'
+            },
+            {
+                label: 'Equipment',
+                icon: 'pi pi-desktop',
+                items: [
+                    {
+                        label: 'Equipment Category',
+                        icon: 'pi pi-desktop'
+                    },
+                    {
+                        label: 'Classic Equipment',
+                        icon: 'pi pi-mobile'
+                    },
+                    {
+                        label: 'Magic Item',
+                        icon: 'pi pi-tablet'
+                    },
+                    {
+                        label: 'Weapon Property',
+                        icon: 'pi pi-tablet'
+                    }
+                ]
+            },
+            {
+                label: 'Character Data',
+                icon: 'pi pi-desktop',
+                items: [
+                    {
+                        label: 'Ability',
+                        icon: 'pi pi-mobile'
+                    },
+                    {
+                        label: 'Alignment',
+                        icon: 'pi pi-desktop'
+                    },
+                    {
+                        label: 'Background',
+                        icon: 'pi pi-tablet'
+                    },
+                    {
+                        label: 'Language',
+                        icon: 'pi pi-tablet'
+                    },
+                    {
+                        label: 'Proficiecy',
+                        icon: 'pi pi-tablet'
+                    },
+                    {
+                        label: 'Skills',
+                        icon: 'pi pi-tablet'
+                    }
+                ]
+            }
+        ]
+    }
 }
